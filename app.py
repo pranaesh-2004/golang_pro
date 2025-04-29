7693911f4175415b030c682dc7fb4b30ef883c7e
Gugan <131288000+Gugan-23@users.noreply.github.com>
2025-04-29 19:39:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -111,5 +111,5 @@ def dashboard():
 if __name__ == "__main__":
     # Get the port from environment variable, default to 5000 if not set
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
 
